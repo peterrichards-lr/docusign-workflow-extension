4186e93e20bf7260b5f9288f120c55f208c006b3
Peter Richards <peter.richards@liferay.com>
2022-09-07 15:38:21 +0100

    Refactoring

--- modules/docusign-mailer/src/main/java/com/liferay/workflow/docusign/action/executor/DocuSignMailer.java
+++ modules/docusign-mailer/src/main/java/com/liferay/workflow/docusign/action/executor/DocuSignMailer.java
@@ -1,15 +1,14 @@
 package com.liferay.workflow.docusign.action.executor;
 
 import com.liferay.digital.signature.manager.DSEnvelopeManager;
-import com.liferay.digital.signature.model.DSEnvelope;
 import com.liferay.digital.signature.model.DSDocument;
+import com.liferay.digital.signature.model.DSEnvelope;
 import com.liferay.digital.signature.model.DSRecipient;
 import com.liferay.document.library.kernel.service.DLAppLocalService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.util.Base64;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.IntegerWrapper;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.kernel.workflow.WorkflowException;
 import com.liferay.portal.kernel.workflow.WorkflowStatusManager;
@@ -31,6 +30,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -92,7 +92,7 @@ public class DocuSignMailer extends BaseWorkflowActionExecutor<DocuSignMailerCon
 
     private boolean sendMail(Map<String, Serializable> workflowContext, DocuSignMailerConfigurationWrapper configuration) throws ActionExecutorException {
         final long companyId = GetterUtil.getLong(workflowContext.get(WorkflowConstants.CONTEXT_COMPANY_ID));
-        final long groupId = GetterUtil.getLong((workflowContext.get( WorkflowConstants.CONTEXT_GROUP_ID)));
+        final long groupId = GetterUtil.getLong((workflowContext.get(WorkflowConstants.CONTEXT_GROUP_ID)));
         final long folderId = configuration.getFolderId();
 
         _log.info("companyId: {}, groupId: {}, folderId: {}", companyId, groupId, folderId);
@@ -111,6 +111,68 @@ public class DocuSignMailer extends BaseWorkflowActionExecutor<DocuSignMailerCon
 
         _log.info("fileEntry: {}", fileEntry);
 
+        /*
+        Based on the following code:
+
+        https://github.com/liferay/liferay-portal/blob/4c2eee0f7807199ec66fa947d6a22dcfc5b186cc/modules/apps/digital-signature/digital-signature-test/src/testIntegration/java/com/liferay/digital/signature/manager/test/DSEnvelopeManagerTest.java
+        https://github.com/liferay/liferay-portal/blob/4c2eee0f7807199ec66fa947d6a22dcfc5b186cc/modules/apps/digital-signature/digital-signature-web/src/main/java/com/liferay/digital/signature/web/internal/portlet/action/AddDSEnvelopeMVCResourceCommand.java
+         */
+
+        final DSDocument document = buildDocument(fileEntry);
+
+        final int recipientId = 1;
+        final String emailAddress = "peter.richards@liferay.com";
+        final String fullName = "Peter Richards";
+        final DSRecipient recipient = buildRecipient(recipientId, emailAddress, fullName);
+
+        final String envelopeName = "Workflow Extensions";
+        final String emailSubject = "DocuSign Example";
+        final String emailBlurb = "This is a test email";
+
+        final String senderEmailAddress = "liferaybotics@liferay.com";
+
+        final DSEnvelope dsEnvelope = buildEnvelope(
+                envelopeName, emailSubject, emailBlurb,
+                Collections.singletonList(recipient),
+                Collections.singletonList(document),
+                senderEmailAddress
+                );
+
+        dsEnvelope.setStatus("sent");
+
+        final DSEnvelope response = dsEnvelopeManager.addDSEnvelope(
+                companyId, groupId,
+                dsEnvelope);
+
+        _log.info("dsEnvelopeId: {}", response.getDSEnvelopeId());
+        _log.info("status: {}", response.getStatus());
+        return true;
+    }
+
+    private DSEnvelope buildEnvelope(final String envelopeName, final String subject, final String blurb, final List<DSRecipient> recipientList, final List<DSDocument> documentList, final String emailAddress) {
+        return new DSEnvelope() {
+            {
+                name = envelopeName;
+                emailSubject = subject;
+                emailBlurb = blurb;
+                dsRecipients = recipientList;
+                dsDocuments = documentList;
+                senderEmailAddress = emailAddress;
+                status = "sent";
+            }
+        };
+    }
+    private DSRecipient buildRecipient(final int recipientId, final String recipientEmailAddress, final String fullName) {
+        return new DSRecipient() {
+            {
+                dsRecipientId = String.valueOf(recipientId);
+                emailAddress = recipientEmailAddress;
+                name = fullName;
+            }
+        };
+    }
+
+    private DSDocument buildDocument(final FileEntry fileEntry) throws ActionExecutorException {
         final byte[] fileEntryData = new byte[(int) fileEntry.getSize()];
         final InputStream inputStream;
         try {
@@ -122,6 +184,14 @@ public class DocuSignMailer extends BaseWorkflowActionExecutor<DocuSignMailerCon
 
         try {
             dis.readFully(fileEntryData);
+            return new DSDocument() {
+                {
+                    data = Base64.encode(fileEntryData);
+                    dsDocumentId = String.valueOf(fileEntry.getFileEntryId());
+                    name = fileEntry.getFileName();
+                    fileExtension = fileEntry.getExtension();
+                }
+            };
         } catch (IOException e) {
             throw new ActionExecutorException("Unable to read content of stream", e);
         } finally {
@@ -131,47 +201,8 @@ public class DocuSignMailer extends BaseWorkflowActionExecutor<DocuSignMailerCon
                 _log.error("Failed to close stream", e);
             }
         }
-
-        /*
-        Based on the following code:
-
-        https://github.com/liferay/liferay-portal/blob/4c2eee0f7807199ec66fa947d6a22dcfc5b186cc/modules/apps/digital-signature/digital-signature-test/src/testIntegration/java/com/liferay/digital/signature/manager/test/DSEnvelopeManagerTest.java
-        https://github.com/liferay/liferay-portal/blob/4c2eee0f7807199ec66fa947d6a22dcfc5b186cc/modules/apps/digital-signature/digital-signature-web/src/main/java/com/liferay/digital/signature/web/internal/portlet/action/AddDSEnvelopeMVCResourceCommand.java
-         */
-
-        final IntegerWrapper integerWrapper = new IntegerWrapper();
-        final DSEnvelope dsEnvelope = dsEnvelopeManager.addDSEnvelope(
-                companyId, groupId,
-        new DSEnvelope() {
-            {
-                dsDocuments = Collections.singletonList(
-                        new DSDocument() {
-                            {
-                                data = Base64.encode(fileEntryData);
-                                dsDocumentId = String.valueOf(fileEntry.getFileEntryId());
-                                name = fileEntry.getFileName();
-                                fileExtension = fileEntry.getExtension();
-                            }
-                        });
-                dsRecipients = Collections.singletonList(
-                        new DSRecipient() {
-                            {
-                                dsRecipientId = String.valueOf(integerWrapper.increment());
-                                emailBlurb = "peter.richards@liferay.com";
-                                name = "Peter Richards";
-                            }
-                        });
-                emailBlurb = "This is a test email";
-                emailSubject = "DocuSign Example";
-                name = "Peter Richards";
-                senderEmailAddress = "liferaybotics@liferay.com";
-                status = "sent";
-            }
-        });
-        _log.info("dsEnvelopeId: {}", dsEnvelope.getDSEnvelopeId());
-        _log.info("status: {}", dsEnvelope.getStatus());
-        return true;
     }
+
 
     private void updateWorkflowStatus(final int status, final Map<String, Serializable> workflowContext) throws WorkflowException {
         try {
